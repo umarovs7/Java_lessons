01c6bda53e4eca520fa25908f30e61019a02df0a
Muhammadyusuf <110180257+1umarovs@users.noreply.github.com>
2026-03-16 09:51:34 +0500

    exercixes for for_loop

---
+++ for_exercise/src/main/java/api/example/Main.java
@@ -0,0 +1,212 @@
+package api.example;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+//        task 1
+
+//        System.out.println("k soni kiriting:");
+//        int k = input.nextInt();
+//        System.out.println("n soni kiriting:");
+//        int  n = input.nextInt();
+//        for (int i =0 ; i < n;i++){
+//            System.out.println("k soni :" + k);
+//        }
+
+
+//        task 2
+
+
+//        System.out.println("a va b soni kirting:");
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//
+//        for(int i=a+1; i < b;i++){
+//            System.out.println(i);
+//        }
+
+
+//        task 3
+
+
+//        System.out.println("a va b soni kirting:");
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//
+//        for(int i=a; i > b;i--){
+//            System.out.println(i);
+//        }
+
+
+
+//        task 4
+
+
+//        System.out.println("konfetni narxini kiriting:");
+//        int narx = input.nextInt();
+//        for(int i = 0 ; i < 10 ; i++){
+//            System.out.println(String.format("%1$s kg konfetning narxi %2$s" , i , narx*i));
+//        }
+
+
+//        task 5
+
+//        System.out.println("Konfetning narxini kiriting:");
+//        int narx = input.nextInt();
+//
+//        for(double i = 0 ; i < 1; i += 0.1 ){
+//            System.out.println(String.format("%1$s kg konfetning narxi %2$s", i , i*narx));
+//        }
+
+
+
+//        task 6
+
+
+//                System.out.println("Konfetning narxini kiriting:");
+//        int narx = input.nextInt();
+//
+//        for(float i = 11 ; i < 20; i ++ ){
+//            System.out.println(String.format("%1$s kg konfetning narxi %2$s", i/10 , i*narx/10));
+//        }
+
+
+//        task 7
+
+
+//        System.out.println("a dan b gacha bolgan son kirting:");
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//        int sum = 0;
+//
+//        for(int i=a; i < b;i++){
+//            sum+=i;
+//        }
+//        System.out.println(sum);
+
+
+//        task 8
+
+
+
+//        System.out.println("a dan b gacha bolgan son kirting:");
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//        int sum = 1;
+//
+//        for(int i=a; i <= b;i++){
+//            sum *=i;
+//        }
+//        System.out.println(sum);
+
+
+
+
+//        task 9
+
+
+//        System.out.println("a dan b gacha bolgan son kiriting:");
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//        int sum = 0;
+//        for (int i = a; i <= b; i++) {
+//            sum += Math.pow(i,2);
+//        }
+//        System.out.println(sum);
+
+
+
+//        task 10
+
+
+//        System.out.println("n butun son kirting:");
+//        int n = input.nextInt();
+//        double s = 0;
+//        for (int i = 1; i <= n; i++) {
+//            s += 1.0/i;
+////            System.out.println(i);
+////            System.out.println(1/n);
+//        }
+//        System.out.println(s);
+
+
+//        task 11
+
+//        System.out.println("n butun son kirting:");
+//        int n = input.nextInt();
+//        double s = 0;
+//        for (int i = 0; i <= n; i++) {
+//            s += Math.pow((n+i),2);
+////            System.out.println(i);
+//            System.out.println(Math.pow((n+i),2));
+//        }
+//        System.out.println(s);
+
+
+
+//        task 12
+
+
+//        System.out.println("n sonini kirting:");
+//        int n = input.nextInt();
+//        float s = 1;
+//        for(int i=1; i<=n;i++){
+//            s *= 1 + i/10.0;
+////            System.out.println(s);
+//        }
+//        System.out.println(s);
+
+
+
+//        task 13
+
+
+
+//        System.out.println("n butun son kiriting:");
+//        int n = input.nextInt();
+//        double s = 0;
+//        for(int i = 1; i <=n ; i++){
+//            s += (1 + i / 10.0 )*Math.pow(-1,i+1);
+////            System.out.println((1 + i / 10.0 )*Math.pow(-1,i+1));
+//        }
+//        System.out.println(s);
+
+
+
+//        task 14
+
+
+//        System.out.println("n sonini kirting:");
+//        int n = input.nextInt();
+//        int s = 0;
+//        for(int i = 1; i<=(2*n-1) ; i+=2){
+//            s += i;
+//        }
+//        System.out.println(s);
+
+
+
+
+//        task 15
+
+
+//        System.out.println("n sonini kiriting:");
+//        int a = input.nextInt();
+//        int n = input.nextInt();
+//        int s = 0;
+//
+//        for(int i=1;i<=n;i++){
+//            System.out.print(a+"*");
+//        }
+
+
+
+
+
+    }
+}
