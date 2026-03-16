90501693fdc655188cc3051a1a63f903d3fc0f17
Muhammadyusuf <110180257+1umarovs@users.noreply.github.com>
2026-03-16 13:40:37 +0500

    methods

---
+++ exercises_for_methods/src/main/java/api/example/Main.java
@@ -0,0 +1,64 @@
+package api.example;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+//        task1
+
+//        System.out.println("Son kiriting va uning 3-darajasini chiqarib beraman:");
+//
+//        int a = scanner.nextInt();
+//
+//        int result = powerA3(a);
+//
+//        System.out.println("Sonning 3-darajasi = " + result);
+//
+//        scanner.close();
+
+
+
+//        task 2
+
+//        System.out.println("son kiriting va uning 2,3,4 darajalarini chiqarib beraman:");
+//        double a = scanner.nextInt();
+//
+//        System.out.println(powerA234(a));
+
+
+//        task 3
+
+
+
+
+    }
+
+
+
+//    for_task1
+
+//    public static int powerA3(int a){
+//        return a * a * a;
+//    }
+
+
+
+//    for_task2
+
+
+
+//    public static String powerA234(double a){
+//        String answer = String.format("%1$s ning 2darajasi = %2$s ,%1$s ning 3darajasi = %3$s ,%1$s ning 4darajasi = %4$s , ", a , a*a,a*a*a,a*a*a*a);
+//        return answer;
+//    }
+//
+
+
+
+
+}
