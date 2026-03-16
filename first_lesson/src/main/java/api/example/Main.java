906685a38d4f14f19e6a6faee89641c6481d8a74
Muhammadyusuf <110180257+1umarovs@users.noreply.github.com>
2026-03-16 09:38:39 +0500

    first_lesson

---
+++ first_lesson/src/main/java/api/example/Main.java
@@ -0,0 +1,197 @@
+package api.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+////        task 1
+//
+//        System.out.print("Kvadratning tomonini kiriting: ");
+//        int a = input.nextInt();
+//        String answer = String.format("Kvadratning perimetri: %1$s", 4*a);
+//        System.out.println(answer);
+
+
+
+////        task 2
+//
+//        System.out.print("kvadratning tomonini kiriting: ");
+//        int a = input.nextInt();
+//        double s = Math.pow(a , 2);
+//        String answer = String.format("Kvadratning yuzi %1$s sm" , s);
+//        System.out.println(answer);
+
+
+
+
+////        task 3
+//
+//        System.out.print("To'g'ri burchakning a tomonini kiriting :");
+//        int a = input.nextInt();
+//        System.out.print("To'g'ri burchakning b tomonini kirting :");
+//        int b = input.nextInt();
+//
+//        int s = a * b ;
+//        int p = 2*(a+b);
+//
+//        String answer = String.format("Kvadratning perimetri %1$s sm , yuzasi %2$s " , p , s);
+//        System.out.println(answer);
+
+
+
+////        task 4
+//
+//
+//        System.out.print("Aylananing diametrini kiriting: ");
+//
+//        double d = input.nextInt();
+//
+//        double l = d * 3.14;
+//
+//        String answer = String.format("Aylananing uzunligi %1$s " , l);
+//        System.out.println(answer);
+
+
+
+//        task 5
+
+
+//        System.out.print("Kubning yon tomoni a ni kiriting : ");
+//        int a =  input.nextInt();
+//
+//        double v = Math.pow(a,3);
+//        double s = Math.pow(a,2) * 6 ;
+//
+//        String answer = String.format("kvadratning hajmi %1$s , yuzasi %2$s" , v,s);
+//        System.out.println(answer);
+
+
+////        task 6
+//
+//
+//        System.out.print("paralolepipedning a,b,c tomonlarini kiriting :");
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//        int c = input.nextInt();
+//
+//        String answer = String.format("paralelopipedning hajmi %1$s , sirti %2$s" , a*b*c , 2*(a*b+a*c+b*c));
+//        System.out.println(answer);
+
+
+
+////        task 7
+//
+//        System.out.println("Doiraning radiusini kiriting: ");
+//        int r = input.nextInt();
+//        double pi = 3.14;
+//        String answer = String.format("Doiraning uzunligi %1$s , yuzasi %2$s ga teng " , 2*pi*r , pi*Math.pow(r,2));
+//        System.out.println(answer);
+
+
+
+////        task 8
+//
+//
+//        System.out.print("Ikkita son kiriting , ularning arfimetigi uchun : ");
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//
+//        String answer = String.format("Ularning Arifmetigi %1$s" , (a+b)/2);
+//        System.out.println(answer);
+
+
+
+////        task 9
+//
+//        System.out.print("ikkita son kiriting ,ularning orta geometrigi uchun:");
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//
+//        String answer = String.format("ularning o'rta geometrigi : %1$s" , Math.sqrt(a*b));
+//        System.out.println(answer);
+
+
+
+////        task 10
+//
+//
+//        System.out.println("ikkita son kiriting ularning kopaytmasi , yigindisi va kvadratlari uchun : ");
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//
+//        String answer = String.format("yigindisi %1$s , kopaytmasi %2$s , kvadratlari %3$s , %4$s" , a+b , a*b , Math.pow(a,2), Math.pow(b,2));
+//        System.out.println(answer);
+
+
+
+//        task 11
+
+
+//        System.out.println("ikkita son kiriting ularning kopaytmasi , yigindisi va kvadratlari uchun : ");
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//
+//        String answer = String.format("yigindisi %1$s , kopaymasi %2$s , modullari %3$s va %4$s ", a+b , a*b , Math.abs(a) , Math.abs(b));
+//
+//        System.out.println(answer);
+
+
+
+//        task 12
+
+
+//        System.out.print("To'g'ri burchakli uchburchakning tomonlari a va b ni kiriting:");
+//        int a = input.nextInt();
+//        int b = input.nextInt();
+//        double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+//        String answer = String.format("Togri burchakli uchbruchakning gipatenuzasi %1$s ga va perimetri %2$s ga teng" , c , a+b+c);
+//        System.out.println(answer);
+
+
+
+//        task 13
+
+
+//        System.out.println("Ikkita aylananing radiuslarini kirting :");
+//        int r1  = input.nextInt();
+//        int r2 = input.nextInt();
+//        double pi = 3.14;
+//
+//        double s1 = pi*r1;
+//        double s2 = pi*r2;
+//        double s3 = pi*(r1-r2);
+//        String answer = String.format("s1 = %1$s , s2 = %2$s , s3 = %3$s ", s1, s2, s3);
+//
+//        System.out.println(answer);
+
+
+
+
+//        task 14
+
+
+//        System.out.println("Aylananing uzunligini toping :");
+//        int l = input.nextInt();
+//        double pi = 3.14;
+//        double r = l/(2*pi);
+//        double s = pi * Math.pow(r,2);
+//        String answer = String.format("radiusi = %1$s , yuzasi = %2$s",r , s);
+//
+//        System.out.println(answer);
+
+
+
+
+//        task 15
+
+
+//        System.out.println("aylananing yuzasini kirting:");
+//        int s = input.nextInt();
+
+
+
+
+    }
+}
