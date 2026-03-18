7367d7008b74e6ef8781348c74bcf9262f681bdc
Muhammadyusuf <110180257+1umarovs@users.noreply.github.com>
2026-03-18 22:51:33 +0500

    start exercises of Arrays

---
+++ array_exercises/src/main/java/api/example/Main.java
@@ -0,0 +1,76 @@
+package api.example;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+//        task 1
+
+
+//        System.out.println("n son kiriting va n ta toq son chiqariladi:");
+//        int n = scanner.nextInt();
+//        int c = 0;
+//        int i = 0;
+//        int[] odd = new int[n];
+//
+//        while(c<n){
+//            i++;
+//            if(i%2==1){
+//                odd[c]= i;
+//                c++;
+//            }
+//        }
+//
+//        for(int num:odd){
+//            System.out.println(num);
+//        }
+
+
+//        task 1.2
+
+//        System.out.println("n son kirting va nta juft son chiqarib beraman:");
+//        int n = scanner.nextInt();
+//
+//
+//        int[] odd = new int[n];
+//        for (int i = 1; i < n; i++) {
+//            odd[i] = 2 * i ;
+//            System.out.println(odd[i]);
+//        }
+
+
+
+//        task 2
+
+
+//        System.out.println("n sonini kirting:");
+//        int n = scanner.nextInt();
+//        double[] pow_of_two = new double[n];
+//
+//        for(int i=0;i<=n;i++){
+//            pow_of_two[i] = Math.pow(2,i);
+//        }
+//
+//
+//        for(double i:pow_of_two){
+//            System.out.println(i);
+//        }
+
+
+//        task 3
+
+
+        
+
+
+    }
+
+
+
+}
